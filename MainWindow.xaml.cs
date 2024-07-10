8d61e05f9e040a1688b196f8b7adc805b21ec1be
daria <dzaytseva@mail.ru>
2024-07-10 20:54:52 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace FixBindingsSample {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+    public class MainViewModel {
+        public string Property { get; set; }
+        public ObservableCollection<Item> Items { get; }
+        public MainViewModel() {
+            Items = new ObservableCollection<Item>();
+            for (int i = 0; i < 100; i++)
+                Items.Add(new Item { Id = i, Name = string.Format("Name_{0}", i) });
+            Property = "Works";
+        }
+    }
+    public class Item {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
